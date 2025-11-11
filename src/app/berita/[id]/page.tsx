492fed96a2b05e4235dedf355d163b0e116f82bf
fauzanunismuh <105841109622@student.unismuh.ac.id>
2025-11-11 08:25:51 +0800

    sd

--- src/app/berita/[id]/page.tsx
+++ src/app/berita/[id]/page.tsx
@@ -1,23 +1,22 @@
 import { getBeritaDetail } from "@/lib/api";
 import { notFound } from "next/navigation";
-import BeritaDetailClient from "./berita-detail-client"; // Impor komponen klien
+import BeritaDetailClient from "./berita-detail-client";
 
 type BeritaDetailParams = {
   id: string;
 };
 
 type BeritaDetailProps = {
-  params: BeritaDetailParams | Promise<BeritaDetailParams>;
+  params: BeritaDetailParams;
 };
 
 export const dynamic = "force-dynamic";
 
 const BeritaDetail = async ({ params }: BeritaDetailProps) => {
   try {
-    const { id } = await params;
+    const { id } = params; // ✅ tanpa await
     const berita = await getBeritaDetail(id);
 
-    // Teruskan data ke komponen klien
     return <BeritaDetailClient berita={berita} />;
   } catch (error) {
     notFound();
